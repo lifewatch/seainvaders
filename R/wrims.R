d04c40cef6b399fb7bae5789d01800d75904cdd7
Pieter Provoost <pieterprovoost@gmail.com>
2019-09-04 15:05:28 +0200

    fetch wrims species

---
+++ R/wrims.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(rgbif)
+library(stringr)
+
+#' Fetches the WRiMS taxa from GBIF.
+#'
+#' @export
+get_wrims_species <- function() {
+  res <- name_usage(datasetKey = "0a2eaf0c-5504-4f48-a47f-c94229029dc8", limit = 10000)
+  taxa <- res$data %>%
+    filter(rank == "SPECIES" & taxonomicStatus == "ACCEPTED") %>%
+    mutate(aphiaid = str_extract(taxonID, "[0-9]+"))
+  return(taxa)
+}
