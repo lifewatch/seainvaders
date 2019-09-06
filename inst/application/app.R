ca9e6d46fc6532bc5e3e1a516ee1683633d0e557
Jelle Rondelez <jeller@vliz.be>
2019-09-06 10:34:32 +0200

    Added about page and layout Added table and images

--- inst/application/app.R
+++ inst/application/app.R
@@ -10,6 +10,9 @@ library(shinythemes)
 library(shinyjs)
 library(shinyalert)
 library(DT)
+library(findingdemo)
+
+rasters <- load_rasters()
 
 
 ######################
@@ -17,53 +20,51 @@ library(DT)
 
 ui <- fluidPage(
 
-    #function for submit-popup
-    useShinyalert(),
+  #function for submit-popup
+  useShinyalert(),
 
-    #Theme of the application
-    theme = shinytheme("cosmo"),
-
-
-    #Tab 1 with information over the concept
-    tabsetPanel(
-        tabPanel("About",
-                 titlePanel("About"),
-                 htmlOutput("Abouttext")),
-
-        #Tab 2 with the content of the application
-        tabPanel("Application",
-                 titlePanel("FindingDemo Application for EMODNet Open Sea Lab"),
-                 br(),
-                 fluidRow(column(width = 7,
-                                 offset = 1,
-                                 actionButton(inputId = "Locationinput",
-                                              label = "Trace location",
-                                              onClick = "shinyjs.geoloc()"),
-                                 h4(textOutput(outputId = "Clickonmap")),
-                                 textOutput(outputId = "Coordinates"),
-                                 br(),
-                                 br(),
-                                 leafletOutput(outputId ="Map"),
-                                 br(),
-                                 tableOutput(outputId = "Invasive")),
+  #Theme of the application
+  theme = shinytheme("cosmo"),
 
 
-                          column(width = 3,
-                                 offset = 1,
-                                 textOutput(outputId = "Introduction"),
-                                 br(),
-                                 br(),
-                                 br(),
-                                 textInput(inputId = "Name",
-                                           label = "Name"),
-                                 textInput(inputId = "Mail",
-                                           label = "Email address"),
-                                 dateInput(inputId = "Date",
-                                           label = "Date of sighting"),
-                                 actionButton(inputId = "Submit",
-                                              label = "SUBMIT"))
-                 ))
-    )
+  #Tab 1 with information over the concept
+  tabsetPanel(
+    tabPanel("About",
+             htmlOutput("Abouttext")),
+
+    #Tab 2 with the content of the application
+    tabPanel("Application",
+             titlePanel("FindingDemo Application for EMODNet Open Sea Lab"),
+             br(),
+             fluidRow(column(width = 3,
+                             offset = 1,
+                             htmlOutput(outputId = "Introduction"),
+                             br(),
+                             textInput(inputId = "Name",
+                                       label = "Name"),
+                             textInput(inputId = "Mail",
+                                       label = "Email address"),
+                             dateInput(inputId = "Date",
+                                       label = "Date of sighting"),
+                             actionButton(inputId = "Submit",
+                                          label = "SUBMIT")),
+                      br(),
+                      br(),
+
+                      column(width = 7,
+                             offset = 1,
+                             actionButton(inputId = "Locationinput",
+                                          label = "Trace location",
+                                          onClick = "shinyjs.geoloc()"),
+                             h4(textOutput(outputId = "Clickonmap")),
+                             textOutput(outputId = "Coordinates"),
+                             br(),
+                             br(),
+                             leafletOutput(outputId ="Map"),
+                             br(),
+                             tableOutput(outputId = "Invasive"))
+             ))
+  )
 )
 
 ########################
@@ -72,113 +73,145 @@ ui <- fluidPage(
 server <- function(input, output) {
 
 
-    # create a character vector of shiny inputs for checkboxes
-    shinyInput = function(FUN, len, id, ...) {
-        inputs = character(len)
-        for (i in seq_len(len)) {
-            inputs[i] = as.character(FUN(paste0(id, i), label = NULL, ...))
-        }
-        inputs
+  # create a character vector of shiny inputs for checkboxes
+  shinyInput = function(FUN, len, id, ...) {
+    inputs = character(len)
+    for (i in seq_len(len)) {
+      inputs[i] = as.character(FUN(paste0(id, i), label = NULL, ...))
     }
+    inputs
+  }
 
-    ####################
-    #### REACTIVITY ####
-    ####################
+  ####################
+  #### REACTIVITY ####
+  ####################
 
-    #Reactive values for user location
-    data_of_click <- reactiveValues(clicked=NULL)
+  #Reactive values for user location
+  data_of_click <- reactiveValues(clicked=NULL)
+  longitude_click <- reactiveValues(lng=NULL)
+  lattitude_click <- reactiveValues(lat=NULL)
 
-    #IF user clicks on map, new coordinates are saved and map is adjusted
-    observeEvent(input$Map_click, {
-        data_of_click$clicked <- input$Map_click
-        leafletProxy('Map') %>%
-            clearMarkers()%>%
-            addMarkers(lng = input$Map_click$lng,
-                       lat = input$Map_click$lat,
-                       popup = paste("Longitude=",round(input$Map_click$lng,2),"and", "Lattitude=", round(input$Map_click$lat,2)))
-    })
-
-    #If user clicks track location its gps location is saved and the map is adjusted
-    observeEvent(input$Locationinput, {
-        leafletProxy('Map') %>%
-            clearMarkers() %>%
-            addControlGPS(options = gpsOptions(position = "topleft", activate = TRUE, autoCenter = TRUE, maxZoom = 60, setView = TRUE))
-        data_of_click$clicked <- input$Map_gps_located
-    })
-
-    #If user submits, a popup is generated
-    observeEvent(input$Submit, {
-        shinyalert(
-            title = "Thanks",
-            text = paste("Sightings saved for", input$Name,"(", input$Mail, ")", "on", input$Date, "at", round(data_of_click$clicked$lng,2),"Longitude","and",round(data_of_click$clicked$lat,2),"Lattitude"),
-            closeOnEsc = TRUE,
-            closeOnClickOutside = TRUE,
-            html = FALSE,
-            type = "success",
-            showConfirmButton = TRUE,
-            showCancelButton = FALSE,
-            confirmButtonText = "Done",
-            confirmButtonCol = "#E0AB8B",
-            imageUrl = "",
-            animation = TRUE
-        )
-    })
+  #IF user clicks on map, new coordinates are saved and map is adjusted
+  observeEvent(input$Map_click, {
+    data_of_click$clicked <- input$Map_click
+    longitude_click <- input$Map_click$lng
+    lattitude_click <- input$Map_click$lat
+    leafletProxy('Map') %>%
+      clearMarkers()%>%
+      addMarkers(lng = input$Map_click$lng,
+                 lat = input$Map_click$lat,
+                 popup = paste("Longitude=",round(input$Map_click$lng,2),"and", "Lattitude=", round(input$Map_click$lat,2)))
+  })
 
 
-    #######################
-    #### VISUALIZATION ####
-    #######################
+  #If user clicks track location its gps location is saved and the map is adjusted
+  observeEvent(input$Locationinput, {
+    leafletProxy('Map') %>%
+      clearMarkers() %>%
+      addControlGPS(options = gpsOptions(position = "topleft", activate = TRUE, autoCenter = TRUE, maxZoom = 60, setView = TRUE))
+    data_of_click$clicked <- input$Map_gps_located
+  })
+
+  #If user submits, a popup is generated
+  observeEvent(input$Submit, {
+    shinyalert(
+      title = "Thanks",
+      text = paste("Sightings saved for", input$Name,"(", input$Mail, ")", "on", input$Date, "at", round(data_of_click$clicked$lng,2),"Longitude","and",round(data_of_click$clicked$lat,2),"Lattitude"),
+      closeOnEsc = TRUE,
+      closeOnClickOutside = TRUE,
+      html = FALSE,
+      type = "success",
+      showConfirmButton = TRUE,
+      showCancelButton = FALSE,
+      confirmButtonText = "Done",
+      confirmButtonCol = "#E0AB8B",
+      imageUrl = "",
+      animation = TRUE
+    )
+  })
 
 
-    #Information underneath location button
-    output$Clickonmap <- renderText({
-        text <- "or click on the map below"
-    })
-
-    #Generation of base map
-    output$Map <- renderLeaflet({
-        map <- leaflet(options = leafletOptions(maxZoom = 8))%>%
-            addProviderTiles("Stamen.Watercolor")
-    })
+  #######################
+  #### VISUALIZATION ####
+  #######################
 
 
-    #Explanatory text in application tab
-    output$Introduction <- renderText({
-        text <- "During three days, in the vibrant and historical city of Ghent (Belgium),
-        teams will compete and bring their expertise to develop novel marine and maritime applications using EMODnet,
-        ICES and Copernicus Marine’s wealth of marine data and services.
-        No previous experience is required! Experts in European marine data management and digital innovation will help build your team (if you don't already have one),
-        lead workshops and coach you to bring your innovative ideas into working solutions."
-        })
+  #Information underneath location button
+  output$Clickonmap <- renderText({
+    text <- "or click on the map below"
+  })
 
-    #Generating mock datatable (needs to be replaced with species)
-    res <- data.table(
-        Species = 1:3,
-        Probability = 1:3,
-        Seen = shinyInput(checkboxInput,3,'Seen_', value = FALSE),
-        Unseen = shinyInput(checkboxInput,3,'Unseen_', value = FALSE),
-        NotChecked = shinyInput(checkboxInput, 3,'Unchecked_', value = FALSE),
-        stringsAsFactors = FALSE)
+  #Generation of base map
+  output$Map <- renderLeaflet({
+    map <- leaflet(options = leafletOptions(minZoom = 3))%>%
+      addProviderTiles("OpenStreetMap.Mapnik")
+    map <- map %>% setView(9, 50, zoom = 4)
+  })
 
-    #Generating table with checkboxes
-    output$Invasive = renderTable({res},
-                                  server = FALSE,
-                                  escape = FALSE,
-                                  selection = 'none',
-                                  options = list(preDrawCallback = JS('function() { Shiny.unbindAll(this.api().table().node()); }'),
-                                                 drawCallback = JS('function() { Shiny.bindAll(this.api().table().node()); } ')),
-                                  sanitize.text.function = function(x) x)
 
-    #Display coordinates in app
-    output$Coordinates <- renderText({
-        paste("Lattitude =", data_of_click$clicked$lat," ---------- ","Longitude =", data_of_click$clicked$lng)
-    })
+  #Explanatory text in application tab
+  output$Introduction <- renderUI({
+  HTML("<p align='justify'>Hi diver!
+    <br>
+    <br>
+    Please provide us your location by clicking 'Trace location' or by pinpointing your location on the map.
+    We will show you a list of harmful invasive species that have not yet been found on your location,
+    but could potentially show up uninvited in the near future. Keep an eye out for these species while diving here!
+    If you see any of the species in this list, check the box and submit to let us know.
+    It is crucial for the health of our ecosystems that we trace these invaders as early as possible.
+    <br>
+    <br>
+    Thank you for protecting our seas!
+    </p>
+    "
+  )
+  })
 
-    #Text on the about page
-    output$Abouttext <- renderUI({
-        HTML()
-    })
-}
+  res <- make_ranking(rasters, 0.5, 53.3)
+  dtable <- data.table(res)
+  dtable[,Seen := shinyInput(checkboxInput,nrow(dtable),'Seen_', value = FALSE)]
+  dtable[,NotSeen := shinyInput(checkboxInput,nrow(dtable),'Unseen_', value = FALSE)]
+  dtable[,Notchecked := shinyInput(checkboxInput, nrow(dtable), "Unchecked_", value = FALSE)]
+
+  output$Invasive <- renderTable({dtable},
+                                server = FALSE,
+                                escape = FALSE,
+                                selection = 'none',
+                                options = list(preDrawCallback = JS('function() { Shiny.unbindAll(this.api().table().node()); }'),
+                                               drawCallback = JS('function() { Shiny.bindAll(this.api().table().node()); } ')),
+                                sanitize.text.function = function(x) x)
+
+  #Display coordinates in app
+  output$Coordinates <- renderText({
+    paste("Lattitude =", data_of_click$clicked$lat," ---------- ","Longitude =", data_of_click$clicked$lng)
+  })
+
+  #Text on the about page
+  output$Abouttext <- renderUI({
+    HTML("<h1><strong>Sea Invaders!</strong></h1>
+
+    <h2>Concept</h2>
+    <p align='justify'>
+    <i>Sea Invaders!</i> provides recreational divers with a 'most wanted' list of invasive species!
+    Based on their location, the service compiles a list of known troublemakers
+    that are most likely to start invading their favourite diving spot.
+    The risk of invasion by given species is based on local habitat suitability and distance to the nearest occurrence.
+    This citizen science tool lets the diving community know what species they need to look out for
+    and acts as a network of early detection for invasive species across Europe.
+    </p>
+
+    <h2>Pipeline</h2>
+    <img src='https://github.com/iobis/findingdemo/blob/master/scheme.png?raw=true' style='width:640px;height:580px;'>
+
+    <h2>Repository</h2>
+    <a href='https://github.com/iobis/findingdemo'>github.com/iobis/findingdemo</a>
+    <br>
+    <br>
+    <br>
+    "
+         )
+  })
+  }
 
 #######################
 ### RUN APPLICATION ###
