a425d40fdbafe440395f99b7678cd4cc00da0cf5
Pieter Provoost <pieterprovoost@gmail.com>
2019-09-06 10:15:52 +0200

    image fix

--- R/ranking.R
+++ R/ranking.R
@@ -18,12 +18,22 @@ load_rasters <- function() {
   return(result)
 }
 
+#' Calculates a score from habitat suitability and distance.
+#'
+#' @export
+calculate_score <- function(hs, d) {
+  score <- hs + ifelse(d > 1000000 & d < 3100000, 0.1, 0) + ifelse(d < 1000000, -0.3, 0)
+  return(score)
+}
+
 #' Make species ranking table.
 #'
 #' @export
 make_ranking <- function(rasters, lon, lat) {
-  ss <- wrims_subset()$aphiaid
-  result <- data.frame(wrims_subset(), d = NA, hs = NA)
+  ws <- wrims_subset() %>%
+    mutate(image = paste0("<img src=\"", image, "\"/>"))
+  ss <- ws$aphiaid
+  result <- data.frame(ws, d = NA, hs = NA)
   for (i in 1:length(ss)) {
     aphiaid <- ss[i]
     d <- extract(rasters[[aphiaid]]$d, cbind(lon, lat))
@@ -31,7 +41,6 @@ make_ranking <- function(rasters, lon, lat) {
     result$d[i] <- d
     result$hs[i] <- hs
   }
-  result$score <- result$hs + ifelse(result$d > 1000000 & result$d < 3100000, 0.1, 0) + ifelse(result$d < 1000000, -0.3, 0)
-  #ggplot(data = result) + geom_point(aes(x = hs, y = d, size = score))
+  result$score <- calculate_score(result$hs, result$d)
   return(result %>% arrange(desc(score)))
 }
